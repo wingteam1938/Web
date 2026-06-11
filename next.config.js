a4bff60b580025614b248d1698025a75a817d584
wingteam1938 <wingteambusiness@gmail.com>
2026-06-11 14:02:56 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.blob.vercel-storage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
